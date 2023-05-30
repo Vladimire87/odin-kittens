c15125b85e366fd74451b4db5c4662f472dca65e
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-05-30 23:12:26 +0400

    Assignment 2 -done

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,51 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitty_params)
+    if @kitten.save
+      flash[:success] = 'Kitten successfully Created.'
+      redirect_to @kitten
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    kitten = Kitten.find(params[:id])
+    kitten.update!(kitty_params)
+    flash[:success] = 'Kitten successfully updates.'
+    redirect_to kitten
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    if @kitten.destroy
+      flash[:success] = 'Kitten successfully destroyed.'
+      redirect_to kittens_path
+    else
+      flash[:error] = 'Failed to destroy the kitten.'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def kitty_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
---
+++ app/models/kitten.rb
@@ -0,0 +1,7 @@
+class Kitten < ApplicationRecord
+  validates :name,
+            :age,
+            :cuteness,
+            :softness,
+            presence: true
+end
